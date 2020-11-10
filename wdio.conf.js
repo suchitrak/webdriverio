34530b23e48edb5ff3e2d48579f2d312988ab641
Suchitra <suchitra.kunnath@gmail.com>
2020-11-10 15:43:36 -0800

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -54,6 +54,10 @@ exports.config = {
         // maxInstances: 5,
         //
         browserName: 'Safari',
+        "safariOptions":
+        {
+            automaticInspection: true
+        }
         // acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
